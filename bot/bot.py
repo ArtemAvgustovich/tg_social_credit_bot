daa714d36964e13aecb9f642ddfac164e053edaa
ArtemAvgustovich <artem.avgustovich@gmail.com>
2021-08-15 13:43:44 +0300

    took webhook bot from https://github.com/aahnik/webhook-aiogram-heroku moved all bot logic and settings to /bot/ added __init__.py with version added settings.py with bot settings temporarily removed database.py added LICENCE added do.py (main file)

--- bot/bot.py
+++ bot/bot.py
@@ -1,15 +1,20 @@
 # *-* encoding utf-8
 import logging
-import os
-from aiogram import Bot, Dispatcher, executor, types
+
+from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
 from constants import add_rating_sticker_id, remove_rating_sticker_id
+from settings import TOKEN, WEBHOOK_PATH, WEBAPP_PORT, WEBAPP_HOST, WEBHOOK_URL
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
-bot = Bot(token=os.environ.get('TOKEN'))
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -42,5 +47,24 @@ async def add_rating(message: types.Message):
     else:
         await message.reply("It seems like you forgot to reply")
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+
+async def on_startup(dispatcher):
+    logging.warning("Starting connection.")
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    logging.warning("Bye! Shutting down webhook connection")
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
